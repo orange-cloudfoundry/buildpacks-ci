0a75fcd61c08ea1a407c121659fdde896bea0125
Olivier ORAND <olivier.orand@orange.com>
2016-06-02 17:50:02 +0200

    identify with TODO, some files with hardcoded url

--- lib/concourse2tracker-resource/spec/concourse2tracker_spec.rb
+++ lib/concourse2tracker-resource/spec/concourse2tracker_spec.rb
@@ -35,6 +35,7 @@ describe 'When a concourse job finishes' do
 
       context 'and the request is successful' do
         before do
+          #TODO concourse URL should be configurable
           @stub = stub_request(:post, 'https://www.pivotaltracker.com/services/v5/projects/987654/stories/1234567/comments')
                  .with(body: { text: 'Concourse pipeline passed: https://buildpacks.ci.cf-app.com/builds/some_build_id' })
                  .with(headers: { 'X-TrackerToken' => '3695', 'Content-Type' => 'application/json' })
@@ -51,6 +52,7 @@ describe 'When a concourse job finishes' do
 
       context 'and the request fails' do
         before do
+          #TODO concourse URL should be configurable
           @stub = stub_request(:post, 'https://www.pivotaltracker.com/services/v5/projects/987654/stories/1234567/comments')
                  .with(body: { text: 'Concourse pipeline passed: https://buildpacks.ci.cf-app.com/builds/some_build_id' })
                  .with(headers: { 'X-TrackerToken' => '3695', 'Content-Type' => 'application/json' })
