225e494847b1dc4e7f66414a575111dcf57c8eed
Olivier ORAND <olivier.orand@orange.com>
2016-08-04 18:29:30 +0200

    add script using orange default value for loading pipelines

---
+++ bin/orange-run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export LPASS_CONCOURSE_PRIVATE_FILE=${LPASS_CONCOURSE_PRIVATE_FILE:-"cw_vdr/concourse-private.yml"}
+export LPASS_DEPLOYMENTS_BUILDPACKS_FILE=${LPASS_DEPLOYMENTS_BUILDPACKS_FILE:-"OrangeShare-Buildpacks/deployments-buildpacks.yml"}
+export LPASS_REPOS_PRIVATE_KEYS_FILE=${LPASS_REPOS_PRIVATE_KEYS_FILE:-"OrangeShare-Buildpacks/buildpack-repos-private-keys.yml"}
+
+export PIPELINE_PREFIX=${PIPELINE_PREFIX:-"buildpack-"}
+
+bin/update-all-the-pipelines
